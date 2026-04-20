892cdb01177b0e77d1f786376a58bc69ffd98f68
phuongnam1909success-del <phuongnam1909.success@gmail.com>
2026-04-20 12:14:41 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+// Centralized configuration for the Payroll WebApp
+
+export const API_URL = "https://script.google.com/macros/s/AKfycbxeeTHOPy-nseJJRRGTibZlDuHv736E4sVaC1DJWbzwtYWv7UyZmwuAh2OWlrsId90r/exec";
+window.API_URL = API_URL;
+
